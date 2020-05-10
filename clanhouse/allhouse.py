1ca1a2d5fc4e840838b442bc49e57c0c00156d1e
20171583沈嘉成 <62537536+Shen1231@users.noreply.github.com>
2020-05-10 22:27:50 +0800

    Add files via upload

---
+++ clanhouse/allhouse.py
@@ -0,0 +1,19 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import numpy as np
+import os
+import csv
+from gyyhouse import *
+from lsyhouse import *
+from lxjhouse import *
+from sjchouse import *
+from syfhouse import *
+from zxshouse import *
+
+if __name__=='__main__':
+    gyyhouse()
+    lsyhouse()
+    lxjhouse()
+    sjchouse()
+    syfhouse()
+    zxshouse()
